59b5709397ac96929f6a6cc13cf6b2949a8bdba6
icarusgk <icarus.gk@gmail.com>
2022-01-01 20:25:46 -0600

    Encapsulate methods in the model and add new routes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,12 @@
 class ApplicationController < ActionController::Base
   def list_posts
-    posts = connection.execute "SELECT * FROM posts"
+    posts = Post.all
 
     render "application/list_posts", locals: { posts: posts }
   end
 
   def show_post
-    post = find_post_by_id(params['id'])
+    post = Post.find(params['id'])
     render "application/show_post", locals: { post: post}
   end
 
@@ -16,47 +16,34 @@ class ApplicationController < ActionController::Base
 
   def create_post
     # Define the query using a heredoc
-    insert_query = <<-SQL
-      INSERT INTO posts (title, body, author, created_at)
-      VALUES (?, ?, ?, ?)
-    SQL
+    post = Post.new 'title' => params['title'],
+                    'body' => params['body'],
+                    'author' => params['author']
 
-    connection.execute insert_query,
-                       params['title'],
-                       params['body'],
-                       params['author'],
-                       Date.current.to_s
-
+    post.save
     redirect_to "/list_posts"
   end
 
   def edit_post
-    post = find_post_by_id(params['id'])
+    post = Post.find(params['id'])
 
     render "application/edit_post", locals: { post: post }
   end
 
-  def find_post_by_id(id)
-    connection.execute("SELECT * FROM posts WHERE posts.id = ? LIMIT 1", id).first
-  end
-
   def update_post
-    update_query = <<-SQL
-      UPDATE posts
-      SET title = ?, body = ?, author = ? WHERE posts.id = ?
-    SQL
+    post = Post.find params['id']
+    post.set_attributes 'title' => params['title'],
+                        'body' => params['body'],
+                        'author' => params['author']
 
-    connection.execute update_query,
-                       params['title'],
-                       params['body'],
-                       params['author'],
-                       params['id']
-
+    post.save
     redirect_to "/list_posts"
   end
 
   def delete_post
-    connection.execute "DELETE FROM posts WHERE posts.id = ?", params['id']
+    post = Post.find params['id']
+    post.destroy
+
     redirect_to "/list_posts"
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
   get "/list_posts", to: "application#list_posts"
   get "/show_post/:id", to: "application#show_post"
+
   get "/new_post", to: "application#new_post"
   post "/create_post", to: "application#create_post"
+
   get "/edit_post/:id", to: "application#edit_post"
   post "/update_post/:id", to: "application#update_post"
+
   post "/delete_post/:id", to: "application#delete_post"
 end
